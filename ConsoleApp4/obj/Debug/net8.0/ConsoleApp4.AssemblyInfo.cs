9b875440aeec27d41aa66374d8f8afe3dda5aac1
Tamerlan Klishev <tamerlan.klishev@axxonsoft.dev>
2024-02-14 20:56:36 +0300

    completed "Stack" task

--- ConsoleApp4/obj/Debug/net8.0/ConsoleApp4.AssemblyInfo.cs
+++ ConsoleApp4/obj/Debug/net8.0/ConsoleApp4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7cf5e20f06c64dcbad2fb136a3da426d18143dc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61e3d58bbebf66da5eca60d3f020c7a61a98a2ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ConsoleApp6/Program.cs
+++ ConsoleApp6/Program.cs
@@ -2,21 +2,79 @@
 
 
 using System.Collections;
+using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics.X86;
 
 Stack<char> chars = new Stack<char>();
+string? s;
+s = Console.ReadLine();
+if (s!= null)
+    for (int i = 0; i < s.Length; i++) chars.Push(s[i]); 
+
+Print<char> (chars);
 
 
+void Print<T>(Stack<T> stack)
+{
+    IEnumerator iterator = stack.GetIterator();
+    Console.Write("Содержимое стэка: ");
+    if (iterator != null)
+    {
+        while (iterator.MoveNext()) Console.Write(iterator.Current());
+        Console.WriteLine();
+    }
+}
+
+interface IEnumerator
+{
+    bool MoveNext();
+    object? Current();
+    void Reset();
+}
+
+interface IEnumerable
+{
+    IEnumerator GetIterator();
+}
+
+class Iterator<T> : IEnumerator
+{
+    private StackItem<T>? pCurItem;
+    private Stack<T> list;
+    public Iterator(Stack<T> _list) => list = _list;
+    public object? Current() => pCurItem == null? null:pCurItem.data;
+
+    public bool MoveNext()
+    {
+        if (list.StackPointer != null) 
+        {
+            if (pCurItem == null)
+            {
+                pCurItem = list.StackPointer;
+                return true;
+            }
+            else if (pCurItem.PrevItem!=null)
+            {
+                pCurItem = pCurItem.PrevItem;
+                return true;
+            }
+            else return false;
+        }
+        else return false;
+    }
+
+    public void Reset() => pCurItem = null;
+}
 record StackItem <T>
 {
-    public T data;
+    public T? data;
     public StackItem<T>? PrevItem;
 
 }
 
-class Stack<T> : IEnumerator<T>, IEnumerable<T>
+class Stack<T> : IEnumerator, IEnumerable
 {
-    private StackItem<T>? StackPointer;
+    public StackItem<T>? StackPointer;
     private StackItem<T>? pCurItem;
     public Stack()
     {
@@ -24,41 +82,46 @@ class Stack<T> : IEnumerator<T>, IEnumerable<T>
     }
     public void Push(T data)
     {
-        throw new NotImplementedException();
+        StackItem<T>? pItem = new StackItem<T>();
+        pItem.data = data;
+        pItem.PrevItem = StackPointer;
+        StackPointer = pItem;
     }
-    public T Pop () 
+    public T? Pop () 
     {
-        throw new NotImplementedException();
+        T? result = default(T);
+        StackItem<T>? pPrevItem = new StackItem<T>();
+        if (StackPointer!= null) 
+        {
+            result = StackPointer.data;
+            pPrevItem = StackPointer.PrevItem;
+            StackPointer = pPrevItem;
+        } 
+        return result;
+        
     }
+    
+    public T? Head() =>StackPointer == null? default(T) : StackPointer.data;
+    
+
     public bool MoveNext()
     {
-        throw new NotImplementedException();
-    }
-    public IEnumerator<T> GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
-
-
-    public void Reset()
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Dispose()
-    {
-        throw new NotImplementedException();
+        if (StackPointer != null)
+        {
+            if (pCurItem == null)
+            {
+                pCurItem = StackPointer;
+                return true;
+            }
+            else if (pCurItem.PrevItem != null) return true;
+            else return false;
+        }
+        else return false;
     }
 
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        throw new NotImplementedException();
-    }
+    public object? Current() => pCurItem == null? null:pCurItem.data;
+    public void Reset() => pCurItem = null;
 
-    public T Current => pCurItem.data;
-
-    object IEnumerator.Current => throw new NotImplementedException();
+    public IEnumerator GetIterator() => new Iterator<T>(this);
 }
-
-
 
