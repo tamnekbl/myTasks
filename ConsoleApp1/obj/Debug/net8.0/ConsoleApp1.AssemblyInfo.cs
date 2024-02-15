784d03740a312cc55875a8a17988c8ae653b5ce2
tamnekbl <kltamerlan13@gmail.com>
2024-02-15 17:52:31 +0300

    completed task "threads"

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b875440aeec27d41aa66374d8f8afe3dda5aac1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+207d6fc200dcbc1f3edb65a456155d3e2228a61e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+ 
+
+
+Param parametr1 = new Param("Main", 200);
+Param parametr2 = new Param("Second", 1000);
+
+Thread myThread = new Thread(Print);
+myThread.Start(parametr2);
+ 
+Print(parametr1);
+ 
+
+Console.WriteLine("Программа завершилась.");
+
+void Print(object? obj)
+{
+    if (obj is Param prt)
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            Console.WriteLine($"Поток {prt.ThreadName}: {i}");
+            Thread.Sleep(prt.SleepValue);
+        }
+        Console.WriteLine($"Вывод из потока {prt.ThreadName} завершился.");
+    }
+    
+}
+public struct Param
+{
+    public string ThreadName;
+    public int SleepValue;
+    public Param(string name, int value)
+    {
+        ThreadName = name;
+        SleepValue = value;
+    }
+}
