273224c0829bcce64ae76699a4a23a6abe588a31
tamnekbl <kltamerlan13@gmail.com>
2024-04-11 17:59:40 +0300

    philosophers changed id

--- ConsoleApp11/obj/Debug/net8.0/ConsoleApp11.AssemblyInfo.cs
+++ ConsoleApp11/obj/Debug/net8.0/ConsoleApp11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+269015573c14ab550d2a40e0930f20cb227209c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b53021700b71981dc5875c32bfe26f7b0718e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ConsoleApp15/Program.cs
+++ ConsoleApp15/Program.cs
@@ -14,7 +14,7 @@ Thread[] philosophers = new Thread[5];
 for (int i = 0; i<5; i++)
 {
     philosophers[i] = new Thread(Guest);
-    philosophers[i].Start(i+1);
+    philosophers[i].Start(i);
 }
 
 void Guest(object? obj)
@@ -43,8 +43,8 @@ void Thinking(int id)
 void TakeForks(int i)
 {
     // Ждем доступности левой вилки
-    forkEvents[i - 1].WaitOne();
-    forks[i - 1] = true; // В этом массиве мы отмечаем, что взяли именно левую вилку
+    forkEvents[i].WaitOne();
+    forks[i] = true; // В этом массиве мы отмечаем, что взяли именно левую вилку
     Console.WriteLine($"Философ {i}: Я взял левую вилку...");
     
     are.WaitOne();  //критическая секция необходима, чтобы вилку уступил только один философ
@@ -52,28 +52,28 @@ void TakeForks(int i)
     for (int j =0; j<5; j++) if (forks[j] == true) flag++;     //если все взяли по левой вилке, значит дедлок => кто-то уступает вилку
     if (flag == 5) 
     {                                                                          
-        forks [i-1] = false;       
-        forkEvents[i - 1].Set(); // Сигнализируем, что левая вилка доступна     
+        forks [i] = false;       
+        forkEvents[i].Set(); // Сигнализируем, что левая вилка доступна     
         System.Console.WriteLine($"Философ {i}: Я положил обратно и уступил левую вилку...");  
         are.Set();  
         Thread.Sleep(100);
-        forkEvents[i - 1].WaitOne();
-        forks[i - 1] = true; // Взяли левую вилку снова  
+        forkEvents[i].WaitOne();
+        forks[i] = true; // Взяли левую вилку снова  
         Console.WriteLine($"Философ {i}: Я взял левую вилку...");                    
     }
     else are.Set();                                       
     
     // Ждем доступности правой вилки
-    forkEvents[i % 5].WaitOne();
+    forkEvents[(4+i) % 5].WaitOne();
     Console.WriteLine($"Философ {i}: Я взял правую вилку и кушаю...");
 }
 
 void PutForks(int i)
 {
     Console.WriteLine($"Философ {i}: Я закончил кушать и положил вилки");
-    forks [i-1] = false;  
-    forkEvents[i - 1].Set(); // Сигнализируем, что левая вилка доступна
-    forkEvents[i % 5].Set(); // Сигнализируем, что правая вилка доступна
+    forks [i] = false;  
+    forkEvents[i].Set(); // Сигнализируем, что левая вилка доступна
+    forkEvents[(4+i) % 5].Set(); // Сигнализируем, что правая вилка доступна
 }
 
 void Lunch()
