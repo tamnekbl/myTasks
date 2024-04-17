c3bdf13df4ad83867bad08e9ead53be8a846f9c1
Tamerlan Klishev <tamerlan.klishev@axxonsoft.dev>
2024-04-17 23:32:00 +0300

    logreader ready?

--- ConsoleApp11/obj/Debug/net8.0/ConsoleApp11.AssemblyInfo.cs
+++ ConsoleApp11/obj/Debug/net8.0/ConsoleApp11.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b53021700b71981dc5875c32bfe26f7b0718e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+273224c0829bcce64ae76699a4a23a6abe588a31")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp11")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Xml.XPath;
+object locker = new(); 
+string dirName = "C://Users//Tamerlan//Desktop//dummy";
+
+int count = 0;
+
+List<Task> tasks = new List<Task>();
+
+List<string> GetAllFiles(string dirName)
+{
+    if (Directory.Exists(dirName))
+    {
+        var nextList = new List<string>();
+        var dirs = Directory.GetDirectories(dirName).ToList();
+        foreach (string s in dirs)
+        {
+            nextList = nextList.Concat(GetAllFiles(s)).ToList();
+        }
+        var files = Directory.GetFiles(dirName).ToList();
+
+        return files.Concat(nextList).ToList();
+    }
+    else return new List<string>();
+}
+
+void LinesCounter (object? obj)
+{
+    if (obj is string dirName)
+    {
+        string? line;
+        TextReader reader = new StreamReader(dirName);
+        
+        while ((line = reader.ReadLine()) != null)
+        {
+        lock (locker)
+        {
+        count++; 
+        }
+            
+        }
+        reader.Close();
+    }
+}
+
+
+
+var files = GetAllFiles(dirName);
+foreach (var file in files)
+{
+    System.Console.WriteLine(file);
+    tasks.Add(Task.Run(()=>LinesCounter(file)));
+}
+
+Task.WaitAll(tasks.ToArray());
+
+System.Console.WriteLine(count);
